cc3344ba36344c3b5c0ce132448ca5f1e2c13e0f
Nazar Turkmen <nazar.rudn@gmail.com>
2020-04-21 10:06:12 +0000

    Automatically backed up by Learn

--- spec/how_old_are_you.rb
+++ spec/how_old_are_you.rb
@@ -1,4 +1,4 @@
-require_relative '.spec/current_age_for_birth_year.rb'
+require_relative 'current_age_for_birth_year.rb'
 
 puts "What year were you born?"
 birth_year = gets.to_i
@@ -6,3 +6,9 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
+
+
+
+
+spec/how_old_are_you.rb:1:in `require_relative': cannot load such file -- /home/redolent-architecture-9667/intro-to-tdd-rspec-and-learn-cb-gh-000/spec
+/.spec/current_age_for_birth_year.rb (LoadError)
